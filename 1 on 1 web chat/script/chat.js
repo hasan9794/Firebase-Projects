2775e44c6dded06878ac3bd9ef0a613e828245ac
hasan9794 <hasanm9794@gmail.com>
2019-02-05 23:38:14 +0500

    chat app working

--- 1 on 1 web chat/script/chat.js
+++ 1 on 1 web chat/script/chat.js
@@ -5,14 +5,14 @@ var config = {
     projectId: "bikeshopsample-38061",
     storageBucket: "bikeshopsample-38061.appspot.com",
     messagingSenderId: "146277578999"
-  };
-  firebase.initializeApp(config);
-  
+};
+firebase.initializeApp(config);
+
 
 window.addEventListener('load', async () => {
     await authCheck()
     loadPreviousData()
-    
+
 })
 
 async function authCheck() {
@@ -20,56 +20,118 @@ async function authCheck() {
     let data = JSON.parse(get)
     console.log(data)
     if (data.user === "null") {
-      document.getElementById("body").innerHTML =
-        `<h1>You don't have permission to access this page. 
+        document.getElementById("body").innerHTML =
+            `<h1>You don't have permission to access this page. 
         Please Login to continue</h1>`
-      return false;
+        return false;
     }
-  }  
-
-function loadPreviousData() {
-    let chatArea = document.getElementById("chatArea");
-    
-    let getcurrentUserId = localStorage.getItem("currentUserId");
-    let currentUserId = JSON.parse(getcurrentUserId)
-    
-    let getclickedUserId = localStorage.getItem("clickedUserId");
-    let clickedUserId = JSON.parse(getclickedUserId)
-
-    let database = firebase.database().ref(`chats/friends/${currentUserId}/${clickedUserId}/messages/`)    
-        database.once('value', (snapshot) => {
-            var message = snapshot.val();
-            if (message.senderReceriver === "sender") {
-                console.log(message.text, message.name);
-            } else {
-                console.log(message.text, message.name);
-            }
-        })
-
-        //getting ready for next step
-        //getting names of 2 user's
-        //display name on top
-        firebase.database().ref(`users/${currentUserId}`)
-            .once('value', (snapshot) =>{
-                let currentUserName = snapshot.val().name;
-                console.log(currentUserName)
-            })
-        firebase.database().ref(`users/${clickedUserId}`)
-            .once('value', (snapshot) =>{
-                let clickedUserName = snapshot.val().name;
-                let topName = document.getElementById("topName");
-                topName.innerHTML = clickedUserName;
-                console.log(clickedUserName)
-            })
 }
 
-function sendMessage() {
+async function loadPreviousData() {
+    let chatArea = document.getElementById("chatArea");
+
     let getcurrentUserId = localStorage.getItem("currentUserId");
     let currentUserId = JSON.parse(getcurrentUserId)
-    
+
+    let getclickedUserId = localStorage.getItem("clickedUserId");
+    let clickedUserId = JSON.parse(getclickedUserId)
+
+    let database = firebase.database().ref(`chats/friends/${currentUserId}/${clickedUserId}/messages/`)
+    database.once('value', (snapshot) => {
+        let messageObj = snapshot.val();
+        let messageKeys = Object.keys(messageObj);
+        console.log(messageKeys)
+        messageKeys.map(key => {
+            if (messageObj[key].senderReceiver === "sender") {
+                chatArea.innerHTML +=
+                `<li class="self">
+                    <div class="msg">
+                    <h5 style="margin:0; padding: 0;">${messageObj[key].name}</h5>
+                    <p></p>
+                    <p>${messageObj[key].text}</p>
+                        <time>20:17</time>
+                    </div>
+                </li>`;
+                // console.log(messageObj[key].text, messageObj[key].name);
+            } else {
+                chatArea.innerHTML +=
+                    `<li class="other">
+                        <div class="msg">
+                        <h5 style="margin:0; padding: 0;">${messageObj[key].name}</h5>
+                        <p>${messageObj[key].text}</p>
+                        <time>20:18</time>
+                        </div>
+                    </li>`
+                // console.log(messageObj[key].text, messageObj[key].name);
+            }
+
+        })
+
+    })
+
+
+    //getting ready for next step
+    //getting names of 2 user's
+    //display name on top
+    firebase.database().ref(`users/${currentUserId}`)
+        .once('value', (snapshot) => {
+            let currentUserName = snapshot.val().name;
+            localStorage.setItem("currentUserName", JSON.stringify(currentUserName))
+            console.log(currentUserName)
+        })
+    firebase.database().ref(`users/${clickedUserId}`)
+        .once('value', (snapshot) => {
+            let clickedUserName = snapshot.val().name;
+            localStorage.setItem("clickedUserName", JSON.stringify(clickedUserName))
+            let topName = document.getElementById("topName");
+            topName.innerHTML = clickedUserName;
+            console.log(clickedUserName)
+        })
+}
+
+
+let sendBtn = document.getElementById('sendBtn')
+sendBtn.addEventListener('click', () => {
+    let messageText = document.getElementById("text").value;
+    let database = firebase.database()
+
+    let getcurrentUserId = localStorage.getItem("currentUserId");
+    let currentUserId = JSON.parse(getcurrentUserId);
+    let getcurrentUserName = localStorage.getItem("currentUserName");
+    let currentUserName = JSON.parse(getcurrentUserName);
+
     let getclickedUserId = localStorage.getItem("clickedUserId");
     let clickedUserId = JSON.parse(getclickedUserId)
     
+    database.ref(`chats/friends/${currentUserId}/${clickedUserId}/messages/`)
+        .push({
+            text: messageText,
+            senderReceiver: "sender",
+            name: currentUserName
+        })
 
 
+    database.ref(`chats/friends/${clickedUserId}/${currentUserId}/messages/`)
+        .push({
+            text: messageText,
+            senderReceiver: "receiver",
+            name: currentUserName
+        })
+})
+
+
+function logOut() {
+    firebase.auth().signOut()
+        .then(() => {
+            localStorage.setItem("userAuth", JSON.stringify({
+                user: "null"
+            }))
+
+            window.location = "../pages/signIn.html"
+            // Sign-out successful.
+        }).catch((error) => {
+            let message = error.message;
+            console.log(message)
+            // An error happened.
+        });
 }
